1c08961d84f9acfdf30be36a02edfb18fb11d076
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-12 21:51:26 +0000

    refactor: update accountEmail to use env environment

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,8 +1,8 @@
 import nodemailer from "nodemailer";
 
-import { EMAIL_PASSWORD } from "../config/env.js";
+import { EMAIL, EMAIL_PASSWORD } from "../config/env.js";
 
-export const accountEmail = "morepints@gmail.com";
+export const accountEmail = EMAIL;
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
