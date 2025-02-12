df1213c7b23da412660c1655f6f50eeaedd79db1
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-12 11:07:04 +0000

    feat: setup database connection

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
-import express from 'express';
+import express from "express";
 
-import { PORT } from './config/env.js';
+import { PORT } from "./config/env.js";
 
-import userRouter from './routes/user.routes.js'
-import authRouter from './routes/auth.routes.js'
-import subscriptionRouter from './routes/subscription.routes.js'
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(PORT, () =>{
-    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
+
+  await connectToDatabase();
 });
