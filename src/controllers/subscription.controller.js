3c0ddb1dfe89a47827b4eca99645eb50d4bd54bd
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-12 16:21:42 +0000

    feat: implement subscription controller

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,38 @@
+import Subscription from "../models/subscription.model.js";
+
+export const createSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Subscription create successfully",
+      data: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this account");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({
+      success: true,
+      message: "User subscriptions fetched successfully",
+      data: subscriptions,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
