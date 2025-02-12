aa43cb4cf8be38839b780473db07e35d5295bf9b
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-12 12:25:54 +0000

    feat: implement login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -50,6 +50,40 @@ export const register = async (req, res, next) => {
   }
 };
 
-export const logIn = async (req, res, next) => {};
+export const logIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      const error = new Error("Invalid email or password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const logOut = async (req, res, next) => {};
