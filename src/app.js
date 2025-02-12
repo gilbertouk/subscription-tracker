d317c387b9e8e27e36612e139785cd446ff6f106
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-12 21:50:15 +0000

    fix: fix typo on import workflowRouter

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
-import workflowsRouter from "./routes/workflow.routes.js";
+import workflowRouter from "./routes/workflow.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
@@ -21,12 +21,12 @@ app.use(arcjetMiddleware);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
-app.use("/api/v1/workflows", workflowsRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to the Subscription Tracker API!");
 });
 
 app.listen(PORT, async () => {
@@ -36,3 +36,5 @@ app.listen(PORT, async () => {
 
   await connectToDatabase();
 });
+
+export default app;
